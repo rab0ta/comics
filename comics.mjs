28a743a768f13cce14d01803db504eaeb483e4e6
rab0ta <aleksei.silber@tptlive.ee>
2024-04-03 08:36:37 +0300

    nvm changed it to hentai images from nhentai

--- comics.mjs
+++ comics.mjs
@@ -6,14 +6,14 @@ if(!fs.existsSync('cache')){
     fs.mkdirSync('cache');
 }
 
-for(let i = 5200; i>5190; i--){
-    let res = await axios.get('https://www.questionablecontent.net/view.php?comic=' + i);
+for(let i = 21; i>0; i--){
+    let res = await axios.get('https://nhentai.to/g/359336/' + i);
     const $ = cheerio.load(res.data);
     let cachePath = `cache/${i}.html`;
     let data;
     if(!fs.existsSync(cachePath)){
         await sleep(2000);
-        let res = await axios.get('https://www.questionablecontent.net/view.php?comic=' + i);
+        let res = await axios.get('https://nhentai.to/g/359336/' + i);
         //CACHE
         fs.writeFileSync(cachePath, res.data);
         data = res.data;
@@ -23,7 +23,7 @@ for(let i = 5200; i>5190; i--){
     }
 
     //comics source output
-    let imgSrc = $('img[id="strip"]').attr('src');
+    let imgSrc = $('section[id="image-container"]>a>img').attr('src');
     
     await sleep(1000)
     console.log(imgSrc);
